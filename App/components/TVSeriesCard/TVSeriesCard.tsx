b143bde7d2eca4219b9012644b75f78c8f18b0e2
aaronjones1 <126199236+aaronjones0@users.noreply.github.com>
2023-10-01 14:34:44 -0400

    UI update

---
+++ App/components/TVSeriesCard/TVSeriesCard.tsx
@@ -0,0 +1,118 @@
+import { Image, Pressable } from 'react-native';
+import Box from '../Box/Box';
+import Text from '../Text/Text';
+import { useEffect, useState } from 'react';
+import { supabase } from '../../../lib/supabase';
+import { TVSeries } from '../../model/TVSeries';
+import { useTheme } from '@shopify/restyle';
+
+export interface TVSeriesCardProps {
+  userId: string;
+  tvSeries: TVSeries;
+  onPress: () => void;
+}
+
+export default function TVSeriesCard({
+  userId,
+  tvSeries,
+  onPress,
+}: TVSeriesCardProps) {
+  const storageBucket = 'tv_series';
+
+  const theme = useTheme();
+  const colors = theme.colors;
+
+  const [keyImageUrl, setKeyImageUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    loadKeyImage(tvSeries.id);
+  }, [tvSeries.id]);
+
+  const loadKeyImage = async (tvSeriesId: string) => {
+    const { data } = await supabase.storage
+      .from(storageBucket)
+      .list(`${userId}/${tvSeriesId}`);
+
+    if (!!data) {
+      const keyImage = data[0];
+      await supabase.storage
+        .from(storageBucket)
+        .download(`${userId}/${tvSeriesId}/${keyImage.name}`)
+        .then(({ data }) => {
+          const fr = new FileReader();
+          fr.readAsDataURL(data!);
+          fr.onload = () => {
+            setKeyImageUrl(fr.result as string);
+          };
+        });
+    } else {
+      setKeyImageUrl(null);
+    }
+  };
+
+  return (
+    <Pressable onPress={() => onPress()}>
+      <Box
+        backgroundColor='backgroundSurface'
+        borderColor='backgroundSurfaceBorder'
+        borderTopWidth={1}
+        borderLeftWidth={1}
+        borderRightWidth={1}
+        borderRadius={24}
+        // padding='s8'
+        shadowColor='shadowColor'
+        shadowOffset={{ width: 0, height: 8 }}
+        shadowOpacity={0.8}
+        shadowRadius={16}
+        marginBottom='s8'
+      >
+        {!!keyImageUrl && (
+          <Box
+            height={150}
+            width='100%'
+            position='relative'
+            overflow='hidden'
+            borderTopStartRadius={24}
+            borderTopEndRadius={24}
+          >
+            <Image
+              style={{
+                position: 'absolute',
+                width: '100%',
+                // width: 150,
+                height: 150,
+                resizeMode: 'cover',
+              }}
+              blurRadius={12}
+              source={{ uri: keyImageUrl }}
+            />
+            <Image
+              style={{
+                position: 'absolute',
+                width: '100%',
+                // width: 150,
+                height: 150,
+                resizeMode: 'contain',
+              }}
+              source={{ uri: keyImageUrl }}
+            />
+          </Box>
+        )}
+        <Box paddingTop='s4' paddingHorizontal='s8' paddingBottom='s8'>
+          <Text variant='h2'>{tvSeries.full_title}</Text>
+          <Text variant='label'>{tvSeries.content_rating}</Text>
+          <Box flexDirection='row' justifyContent='space-between'>
+            <Box flexDirection='row'>
+              <Text variant='label'>Seasons: </Text>
+              <Text variant='body'>{tvSeries.season_count}</Text>
+            </Box>
+            <Box flexDirection='row'>
+              <Text variant='label'>Episodes: </Text>
+              <Text variant='body'>{tvSeries.episode_count}</Text>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Pressable>
+  );
+}
--- App/views/TVSeriesCollection/TVSeriesCollection.tsx
+++ App/views/TVSeriesCollection/TVSeriesCollection.tsx
@@ -1,4 +1,10 @@
-import { Alert, Pressable, RefreshControl, ScrollView } from 'react-native';
+import {
+  Alert,
+  Image,
+  Pressable,
+  RefreshControl,
+  ScrollView,
+} from 'react-native';
 import Box from '../../components/Box/Box';
 import Text from '../../components/Text/Text';
 import { useEffect, useState } from 'react';
@@ -6,8 +12,11 @@ import { TVSeries } from '../../model/TVSeries';
 import { useAuth } from '../../../lib/providers/AuthProvider';
 import { supabase } from '../../../lib/supabase';
 import AddFAB from '../../components/AddFAB/AddFAB';
+import TVSeriesCard from '../../components/TVSeriesCard/TVSeriesCard';
 
 export default function TVSeriesCollection({ navigation }) {
+  const storageBucket = 'tv_series';
+
   const { user } = useAuth();
   const [loading, setLoading] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
@@ -38,6 +47,28 @@ export default function TVSeriesCollection({ navigation }) {
     }
   };
 
+  const loadKeyImage = async (tvSeriesId: string): Promise<string | null> => {
+    const { data } = await supabase.storage
+      .from(storageBucket)
+      .list(`${user.id}/${tvSeriesId}`);
+
+    if (!!data) {
+      const keyImage = data[0];
+      await supabase.storage
+        .from(storageBucket)
+        .download(`${user.id}/${tvSeriesId}/${keyImage.name}`)
+        .then(({ data }) => {
+          const fr = new FileReader();
+          fr.readAsDataURL(data!);
+          fr.onload = () => {
+            return fr.result as string;
+          };
+        });
+    } else {
+      return null;
+    }
+  };
+
   return (
     <Box height='100%' backgroundColor='backgroundSurface'>
       <Box position='absolute' bottom={56} alignSelf='center' zIndex={10}>
@@ -45,7 +76,7 @@ export default function TVSeriesCollection({ navigation }) {
       </Box>
       {!!tvSeries ? (
         <ScrollView
-          style={{ padding: 24 }}
+          style={{ paddingTop: 24, paddingHorizontal: 24, paddingBottom: 96 }}
           refreshControl={
             <RefreshControl
               refreshing={refreshing}
@@ -54,40 +85,48 @@ export default function TVSeriesCollection({ navigation }) {
           }
         >
           {tvSeries.map((item, index) => (
-            <Pressable
+            // <Pressable
+            //   key={item.id}
+            //   onPress={() =>
+            //     navigation.navigate('TV Series Detail', { tvSeriesId: item.id })
+            //   }
+            // >
+            //   <Box
+            //     backgroundColor='backgroundSurface'
+            //     borderColor='backgroundSurfaceBorder'
+            //     borderTopWidth={1}
+            //     borderLeftWidth={1}
+            //     borderRightWidth={1}
+            //     borderRadius={24}
+            //     padding='s8'
+            //     shadowColor='shadowColor'
+            //     shadowOffset={{ width: 0, height: 8 }}
+            //     shadowOpacity={0.8}
+            //     shadowRadius={16}
+            //     marginBottom='s8'
+            //   >
+            //     <Text variant='h2'>{item.full_title}</Text>
+            //     <Text variant='label'>{item.content_rating}</Text>
+            //     <Box flexDirection='row' justifyContent='space-between'>
+            //       <Box flexDirection='row'>
+            //         <Text variant='label'>Seasons: </Text>
+            //         <Text variant='body'>{item.season_count}</Text>
+            //       </Box>
+            //       <Box flexDirection='row'>
+            //         <Text variant='label'>Episodes: </Text>
+            //         <Text variant='body'>{item.episode_count}</Text>
+            //       </Box>
+            //     </Box>
+            //   </Box>
+            // </Pressable>
+            <TVSeriesCard
               key={item.id}
               onPress={() =>
                 navigation.navigate('TV Series Detail', { tvSeriesId: item.id })
               }
-            >
-              <Box
-                backgroundColor='backgroundSurface'
-                borderColor='backgroundSurfaceBorder'
-                borderTopWidth={1}
-                borderLeftWidth={1}
-                borderRightWidth={1}
-                borderRadius={24}
-                padding='s8'
-                shadowColor='shadowColor'
-                shadowOffset={{ width: 0, height: 8 }}
-                shadowOpacity={0.8}
-                shadowRadius={16}
-                marginBottom='s8'
-              >
-                <Text variant='h2'>{item.full_title}</Text>
-                <Text variant='label'>{item.content_rating}</Text>
-                <Box flexDirection='row' justifyContent='space-between'>
-                  <Box flexDirection='row'>
-                    <Text variant='label'>Seasons: </Text>
-                    <Text variant='body'>{item.season_count}</Text>
-                  </Box>
-                  <Box flexDirection='row'>
-                    <Text variant='label'>Episodes: </Text>
-                    <Text variant='body'>{item.episode_count}</Text>
-                  </Box>
-                </Box>
-              </Box>
-            </Pressable>
+              tvSeries={item}
+              userId={user.id}
+            />
           ))}
         </ScrollView>
       ) : (
